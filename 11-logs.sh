7c52d3dcf2189683d585fb0b2fc541f7701486e4
vasim <shaikwaseem1001@gmail.com>
2024-08-10 19:09:18 +0530

    log

---
+++ 11-logs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "script name: $0"
+ID=$(id -u)
+VALIDATE(){    
+    if [ $1 -ne 0 ]
+    then 
+        echo "error: $2 ... failed"
+        exit 1
+    else
+        echo "$2 ...  success"
+    fi
+}
+ID=$(id -u)
+if [ $ID -ne 0 ]
+then 
+   echo "ERROR:: please run this script with root access"
+else
+   echo "it is root user"
+fi
+yum install mysql -y
+VALIDATE $? "installing mysql"
+yum install git -y
+VALIDATE $? "installing git"
