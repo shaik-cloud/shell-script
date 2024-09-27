d2d65759ba856e0ab64453dfce680f265a16f46b
vasim <shaikwaseem1001@gmail.com>
2024-09-27 09:16:39 +0530

    loops

--- 13-Install-Packages.sh
+++ 13-Install-Packages.sh
@@ -3,6 +3,20 @@ ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
 if [ $ID -ne 0 ]
 then 
     echo -e "$R ERROR :: please run this script with roor access $N"
@@ -10,4 +24,16 @@ then
 else 
     echo -e "$G you are root user $N"
 fi
-echo "all arguments passed : $@"
+#echo "All arguments passed: $@"
+# git mysql postfix net-tools
+# package=git for first time
+for package in $@
+do
+    yum list installed $package &>> LOGFILE #checks package installed or not
+if [ $? -ne 0]
+then
+    yum install $package -y &>> LOGFILE      #installs the package
+    VALIDATE $? "installation of $package"   #validate
+    
+
+
